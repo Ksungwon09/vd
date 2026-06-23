4de3a76c080fc6830993fe9169b8e6b2eb084fc7
sungwon Kang <sutingstar19@gmail.com>
2026-06-24 02:29:31 +0900

    Persist database in data volume

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     jwt_algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
     initial_admin_username: str = "admin"
-    database_url: str = "sqlite+aiosqlite:///./vdownloader.db"
+    database_url: str = "sqlite+aiosqlite:///./data/vdownloader.db"
 
     class Config:
         env_file = ".env"
